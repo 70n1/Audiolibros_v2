caa0c86a23e20114c656d44b2430a9b2b4d2d657
70n1 <el.70n1@gmail.com>
2016-12-26 11:27:46 +0100

    Incorporando búsquedas dinámicamente

--- app/src/main/java/com/example/audiolibros/fragments/SelectorFragment.java
+++ app/src/main/java/com/example/audiolibros/fragments/SelectorFragment.java
@@ -7,9 +7,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -110,6 +112,34 @@ public class SelectorFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_selector, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_buscar);
+        SearchView searchView= (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String query) {
+                adaptador.setBusqueda(query);
+                adaptador.notifyDataSetChanged();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+        });
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                adaptador.setBusqueda("");
+                adaptador.notifyDataSetChanged();
+                return true; // Para permitir cierre
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true; // Para permitir expansión
+            }
+        });
         super.onCreateOptionsMenu(menu, inflater);
     }
 
