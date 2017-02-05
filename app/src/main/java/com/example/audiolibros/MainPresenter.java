f01766457a189c20f3ee109891cda7bd59895461
70n1 <el.70n1@gmail.com>
2017-02-05 09:39:00 +0100

    Uso de Repositorio para abstraer

--- app/src/main/java/com/example/audiolibros/MainPresenter.java
+++ app/src/main/java/com/example/audiolibros/MainPresenter.java
@@ -7,17 +7,19 @@ package com.example.audiolibros;
 public class MainPresenter {
     private final LibroStorage libroStorage;
     private final SaveLastBook saveLastBook;
+    private final BooksRespository booksRespository;
     private final View view;
 
     public MainPresenter(SaveLastBook saveLastBook, LibroStorage libroStorage, MainPresenter.View view) {
         this.saveLastBook = saveLastBook;
         this.libroStorage = libroStorage;
+        this. booksRespository = new BooksRespository(libroStorage);
         this.view = view;
     }
 
     public void clickFavoriteButton() {
         if (libroStorage.hasLastBook()) {
-            view.mostrarFragmentDetalle(libroStorage.getLastBook());
+            view.mostrarFragmentDetalle(booksRespository.getLastBook());
         } else {
             view.mostrarNoUltimaVisita();
         }
