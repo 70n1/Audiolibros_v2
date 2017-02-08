a6b639c4e428eed6f8b304b33a99d2497ee76054
70n1 <el.70n1@gmail.com>
2017-02-08 16:27:05 +0100

    Configurar la base datos y referencias.

--- app/src/main/java/com/example/audiolibros/Aplicacion.java
+++ app/src/main/java/com/example/audiolibros/Aplicacion.java
@@ -8,6 +8,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Vector;
 
@@ -19,13 +21,24 @@ public class Aplicacion extends Application {
 
 
     private FirebaseAuth auth;
+    private final static String BOOKS_CHILD = "libros";
+    private final static String USERS_CHILD = "usuarios";
+    private DatabaseReference usersReference;
 
     @Override
     public void onCreate() {
         auth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setPersistenceEnabled(true);
+        DatabaseReference booksReference = database.getReference().child(BOOKS_CHILD);
+        usersReference = database.getReference().child(USERS_CHILD);
     }
 
     public FirebaseAuth getAuth() {
         return auth;
+    }
+
+    public DatabaseReference getUsersReference() {
+        return usersReference;
     }
 }
